069c87f78cd3faef016df351ea8e20b67b221090
zzining <iammes98@gmail.com>
2023-08-23 17:52:29 +0900

    q

--- html-ktown4u/dist/js/comm.js
+++ html-ktown4u/dist/js/comm.js
@@ -1,13 +1,37 @@
-var swiper = new Swiper(".slide-intro", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+// var swiper = new Swiper(".mySwiper", {
+//   spaceBetween: 30,
+//   // effect: "fade",
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
+// const progressCircle = document.querySelector(".autoplay-progress svg");
+// const progressContent = document.querySelector(".autoplay-progress span");
+// var swiper = new Swiper(".mySwiper", {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   on: {
+//     autoplayTimeLeft(s, time, progress) {
+//       progressCircle.style.setProperty("--progress", 1 - progress);
+//       progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+//     },
+//   },
+// });
